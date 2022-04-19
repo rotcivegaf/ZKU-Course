07c92d1e957b2757896b75077b5f5b1f7c16d102
rotcivegaf <victorfage@gmail.com>
2022-04-19 16:54:19 -0300

    Background Assignment submit

---
+++ HelloWorld.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title HelloWorld
+ * @dev Set & get number
+ */
+contract HelloWorld {
+    event StoreNumber(uint256 number);
+
+    uint256 internal number;
+
+    /**
+     * @dev Store/Set the number
+     * @param _number an unsigned integer 256
+     */
+    function storeNumber(uint256 _number) external {
+        number = _number;
+        emit StoreNumber(_number);
+    }
+
+    /**
+     * @dev Return number
+     * @return unsigned integer 256
+     */
+    function retrieveNumber() external view returns(uint256) {
+        return number;
+    }
+}
